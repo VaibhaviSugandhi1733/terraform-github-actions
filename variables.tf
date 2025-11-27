daf99e324a1e64fb3ed0de0f6706f4f60aaf9a0b
vaibhavi <sugandhivaibhavi1733@gmail.com>
2025-11-27 15:12:08 +0530

    yaml change3

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "vaibhavi-practice-bucket-2004"
+  default     = "vaibhavi-practice-bucket-2005"
 }
 
 variable "bucket_tags" {
   description = "Tags for S3 bucket"
   type        = map(string)
   default = {
-    Name        = "vaibhavi-practice-bucket-2004"
+    Name        = "vaibhavi-practice-bucket-2005"
     Environment = "Dev"
   }
 }
@@ -16,7 +16,7 @@ variable "bucket_tags" {
 variable "iam_user_name" {
   description = "Name of the IAM user"
   type        = string
-  default     = "vaibhavi2004"
+  default     = "vaibhavi2005"
 }
 
 variable "iam_user_path" {
